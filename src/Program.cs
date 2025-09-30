f5fc0d9aeebb97de34a7b6629e08ecdded72afc9
Andrew DeSimone <andrewdesimone96@gmail.com>
2025-09-29 22:25:22 -0500

    Setup environment

--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,32 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using App.Infrastructure;
+
+DotNetEnv.Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var host = Environment.GetEnvironmentVariable("PG_HOST") ?? "localhost";
+var port = Environment.GetEnvironmentVariable("PG_PORT") ?? "5432";
+var database = Environment.GetEnvironmentVariable("PG_DATABASE") ?? "authdb";
+var username = Environment.GetEnvironmentVariable("PG_USERNAME") ?? "postgres";
+var password = Environment.GetEnvironmentVariable("PG_PASSWORD") ?? "";
+
+var connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
 
 app.Run();
